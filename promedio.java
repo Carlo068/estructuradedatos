53b907a8d1d2537a28c5deea0fee29430f538925
Carlo068 <carlo.ruiz.scch@gmail.com>
2025-02-05 10:44:17 -0600

    add comments to clarify calculations in promedio class

--- promedio.java
+++ promedio.java
@@ -5,11 +5,14 @@
 
 public class promedio {
     public static void main(String[] args) {
+        // Se crea un arreglo double para guardar las calificaciones
         double[] calificaciones = new double[]{10, 9, 8};
         double sum = 0;
+        // El for sirve para sumar todas las calificaciones
         for (int i = 0; i < calificaciones.length; i++) {
             sum = calificaciones[i] + sum;
         }
+        // avg guarda el promedio
         double avg = sum / 3;
         System.out.println(avg);
     }
