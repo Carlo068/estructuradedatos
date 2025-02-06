345a1cb55df0e249b80ea1ed201f6528ddff3901
Carlo068 <carlo.ruiz.scch@gmail.com>
2025-02-06 10:43:04 -0600

    add documentation and comments to UserData class for fullNameString method

--- UserData.java
+++ UserData.java
@@ -1,5 +1,12 @@
+/** Carlo Alejandro Ruiz OLvera
+* 14961
+* fecha 06/02/2025
+*/
+
 public class UserData {
+    // Se crea una funcion que recibe dos strings y regresa un string con el nombre completo
     public String fullNameString(String firstName, String lastName) {
+        // Se crea un string que guarda el nombre completo
         String fullName = String.format("%s %s", firstName, lastName);
         return fullName;
     }
