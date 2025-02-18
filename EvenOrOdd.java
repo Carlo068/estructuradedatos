38a8029538e533e1eda69803853e6250ba5f6900
Carlo068 <carlo.ruiz.scch@gmail.com>
2025-02-18 10:50:51 -0600

    update EvenOrOdd class to include zero count and enhance comments for clarity

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,9 +1,14 @@
+/** Carlo Alejandro Ruiz OLvera
+* 14961
+* fecha 18/02/2025
+*/
+
 public class EvenOrOdd {
     public void evenorodd(int[] numbers){
         int even = 0;
         int odd = 0;
         int zeros = 0;
-        // Se verifica si el número es par o impar
+        // Se verifica si el número es par o impar o cero
         for (int number : numbers) {
             if (number == 0) {
                 zeros++;
@@ -13,6 +18,7 @@ public class EvenOrOdd {
                 odd++;
             }
         }
+        // Se imprime el resultado
         System.out.println("Total de Pares: " + even);
         System.out.println("Total de Impares: " + odd);
         System.out.println("Total de Ceros: " + zeros);
